a8d75f25eacd4904c91de4fdc70d97b408f1c570
Ra.khaihan <rakharaihan2244@gmail.com>
2025-05-12 18:42:43 +0700

    menambahkan automata dan table driven

---
+++ Tubes_Console/Service/SiswaService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tubes_Console.Model;
+
+namespace Tubes_Console.Service
+{
+    public class SiswaService
+    {
+        private List<Siswa> _daftarSiswa = new();
+
+        public void TambahSiswa(Siswa siswa) => _daftarSiswa.Add(siswa);
+
+        public Siswa? CariSiswa(int id) => _daftarSiswa.FirstOrDefault(s => s.Id == id);
+
+        public List<Siswa> GetSemua()
+        {
+            return _daftarSiswa;
+        }
+
+    }
+}
---
+++ Tubes_Console/table driven/LevelPelanggaran.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes_Console.table_driven
+{
+    public enum LevelPelanggaran
+    {
+        RINGAN,
+        SEDANG,
+        BERAT
+    }
+}
