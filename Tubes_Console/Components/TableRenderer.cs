fb98fa51e4f4df20cdf51b74bfa051c31680064a
azzkiabdl <147375620+azzkiabdl@users.noreply.github.com>
2025-05-13 14:49:34 +0700

    Memperbaiki code

--- Tubes_Console/Components/TableRenderer.cs
+++ Tubes_Console/Components/TableRenderer.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tubes_Tahap1_KPL_kelompok3.Components
+namespace Tubes_Console.Components
 {
     public static class TableRenderer
     {
--- Tubes_Console/obj/Debug/net8.0/Tubes_Console.AssemblyInfo.cs
+++ Tubes_Console/obj/Debug/net8.0/Tubes_Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tubes_Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c640fc4683a51e95e497d8c93c35d1c1661ffe9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tubes_Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tubes_Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
