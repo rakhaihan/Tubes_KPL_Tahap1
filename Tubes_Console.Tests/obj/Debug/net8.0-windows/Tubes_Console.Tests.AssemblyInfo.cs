df9c657b4ede8951d0c77c9c9beeb64c88470bbe
Zipooxy <142865402+Zipooxy@users.noreply.github.com>
2025-05-14 19:41:28 +0700

    menambahkan unit test config

--- Tubes_Console.Tests/obj/Debug/net8.0-windows/Tubes_Console.Tests.AssemblyInfo.cs
+++ Tubes_Console.Tests/obj/Debug/net8.0-windows/Tubes_Console.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tubes_Console.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcea3a74951e098266978dbb4879b627ad88acd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c761640667fbb342784a1dce1298ff395b384e74")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tubes_Console.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tubes_Console.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
